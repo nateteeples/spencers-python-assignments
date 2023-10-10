12fddd6a4c799dd66763dda42bc6e58dbf0a8172
nateteeples <87500608+nateteeples@users.noreply.github.com>
2023-10-10 10:44:45 -0600

    Create sum100.py

---
+++ sum100.py
@@ -0,0 +1,8 @@
+value1 = int(input("Input first value: "))
+value2 = int(input("Input second value: "))
+if (value1 + value2) > 100:
+    print("Sum is greater than 100")
+elif (value1 + value2) < 100:
+    print("Sum is less than 100")
+else:
+    print("Sum is equal to 100")
