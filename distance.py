00b5e85313a5caed43f141df651144634fde282d
nateteeples <87500608+nateteeples@users.noreply.github.com>
2023-10-10 20:22:14 -0600

    Create distance.py

---
+++ distance.py
@@ -0,0 +1,4 @@
+speed = int(input("Input travel speed (mph): "))
+time = int(input("Input time travelled (minutes): "))
+distance = (speed / 60) * time
+print(f"Distance travelled: {distance} miles")
